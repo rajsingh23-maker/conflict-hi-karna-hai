e50006cb310b44585e33ed4c15a1de8b30aa200a
raj.singh23 <raj.singh23@spit.ac.in>
2026-02-16 14:26:58 +0530

    first commit

---
+++ code.py
@@ -0,0 +1,18 @@
+def subtract(a, b):
+    return a + b
+
+
+def main():
+    print("Subtraction Program")
+    
+    # Taking input from user
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    
+    result = subtract(num1, num2)
+    
+    print("Result:", result)
+
+
+if __name__ == "__main__":
+    main()
